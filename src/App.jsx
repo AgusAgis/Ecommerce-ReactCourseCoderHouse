7a1c777ece3acf7718bb9c2a1bd0a6fe0676291f
AgustinAgis <agustin.agis@gmail.com>
2024-09-28 12:40:29 -0300

    imagesAdded

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home greeting={"Todo en un solo lugar"} />} />
           <Route path="alimentos" element={<ItemListContainer />} /> 
-          <Route path="alimentos/:idCategory" element={<ItemListContainer />} /> 
+          <Route path="alimentos/:idCategory" element={<ItemListContainer />} />           
           <Route path= "alimentos/detail/:idProduct" element={<ItemDetailContainer/>}/>
           <Route path="snacks" element={<Snacks />} />  
           <Route path="accesorios" element={<Accesorios />} />  
