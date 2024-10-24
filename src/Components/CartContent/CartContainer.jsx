0ab7c4631bd59d1125aca955413ba6888216d37f
AgustinAgis <agustin.agis@gmail.com>
2024-10-23 21:17:50 -0300

    conditionalRenderingCartContainer

--- src/Components/CartContent/CartContainer.jsx
+++ src/Components/CartContent/CartContainer.jsx
@@ -1,11 +1,23 @@
 import CartElements from "./CartElements";
 import CartTotal from "./CartTotal";
+import { useContext } from "react";
+import { dataContext } from "../../context";
+
 const CartContainer = () =>{
-    return(
+    const { cart } = useContext(dataContext)
+
+    return cart.length > 0 ? (
         <>
         <CartElements/>
         <CartTotal/>
         </>
-    ) 
+    ) : (
+        <>
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '65vh' }}>
+            <h2 className="text-center mb-4">Tu carrito está vacío</h2>
+            <img src="/images/perro-triste.jpg" alt="perrito triste" style={{ width: '50%', maxWidth: '500px', height: 'auto' }} />
+        </div>
+        </>
+    )
 }
 export default CartContainer;
