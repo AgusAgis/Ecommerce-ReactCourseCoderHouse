1daeeaa40ac9d9431aaf92b84152587db48ac7c9
AgustinAgis <agustin.agis@gmail.com>
2024-10-25 15:38:27 -0300

    form

---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,60 @@
+const Form = () => {
+  return (
+    <form>
+        <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">
+            Nombre
+            </label>
+            <input
+            type="text"
+            className="form-control"
+            id="exampleInputName"
+            />
+        </div>
+        <div className="mb-3">
+            <label htmlFor="exampleInputSurname" className="form-label">
+            Apellido
+            </label>
+            <input
+            type="text"
+            className="form-control"
+            id="exampleInputSurname"
+            />
+        </div>
+        <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+            Email
+            </label>
+            <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            />
+        </div>
+        <div className="mb-3">
+            <label htmlFor="exampleInputSurname" className="form-label">
+            Whatsapp
+            </label>
+            <input
+            type="text"
+            className="form-control"
+            id="exampleInputSurname"
+            />
+        </div>      
+        <div className="mb-3 form-check">
+            <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+            Acepto términos de uso
+            </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+            Crear pedido
+        </button>
+    </form>
+  );
+};
