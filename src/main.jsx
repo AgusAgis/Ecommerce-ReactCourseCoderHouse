5d8cd3d17e397dd71ff01c22f8e7b6dded1fa8d2
AgustinAgis <agustin.agis@gmail.com>
2024-10-25 10:30:01 -0300

    variablesDeEntorno

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,12 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHQAg7d_lc0oBOKg3IIXUWz8kgRxmhk9o",
-  authDomain: "ecommerce-mascotitas.firebaseapp.com",
-  projectId: "ecommerce-mascotitas",
-  storageBucket: "ecommerce-mascotitas.appspot.com",
-  messagingSenderId: "606944269839",
-  appId: "1:606944269839:web:725866e0d289431140f57a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
